bcee4d0fe84bb35d22cb6d83db32903f29e30e8e
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-11-20 18:37:40 +0200

    task 6

--- JS/test_module_3.js
+++ JS/test_module_3.js
@@ -47,6 +47,59 @@
 // ==============================================3/41=========================================
 
 // ==============================================4/41=========================================
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length-1];
+// // Change code above this line
+// console.log(ownerName);
+// console.log(ownerPhone);
+// console.log(ownerEmail);
+// console.log(numberOfTags);
+// console.log(firstTag);
+// console.log(lastTag);
+// ==============================================4/41=========================================
+
+// ==============================================5/41=========================================
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = apartment["rating"];
+// const aptDescr = apartment["descr"];
+// const aptPrice = apartment["price"];
+// const aptTags = apartment["tags"];
+// // Change code above this line
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+// ==============================================5/41=========================================
+
+// ==============================================6/41=========================================
+
+
 const apartment = {
   imgUrl: "https://via.placeholder.com/640x480",
   descr: "Spacious apartment in the city center",
@@ -61,19 +114,9 @@ const apartment = {
 };
 
 // Change code below this line
-const ownerName = apartment.owner.name;
-const ownerPhone = apartment.owner.phone;
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags.length;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[apartment.tags.length-1];
-// Change code above this line
-console.log(ownerName);
-console.log(ownerPhone);
-console.log(ownerEmail);
-console.log(numberOfTags);
-console.log(firstTag);
-console.log(lastTag);
-// ==============================================4/41=========================================
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = "Henry Sibola";
+apartment.tags.push("trusted");
 
-// ==============================================5/41=========================================
+console.log(apartment);
