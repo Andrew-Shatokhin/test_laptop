f483fe5dfaf926ca2548111f04dfed60308361e4
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-11-25 12:00:37 +0200

    task 19

--- JS/test_module_3.js
+++ JS/test_module_3.js
@@ -345,6 +345,34 @@
 
 // ==============================================18/41=========================================
 
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+    
+// }
+// return null;
+
+//   // Change code above this line
+// };
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+// ==============================================18/41=========================================
+
+// ==============================================19/41=========================================
+
 const products = [
   { name: "Radar", price: 1300, quantity: 4 },
   { name: "Scanner", price: 2700, quantity: 3 },
@@ -352,21 +380,23 @@ const products = [
   { name: "Grip", price: 1200, quantity: 9 },
 ];
 
-function getProductPrice(productName) {
+function getAllPropValues(propName) {
   // Change code below this line
+  let propValue = [];
+  
   for (product of products) {
-    if (product.name === productName) {
-      return product.price;
+    if (Object.keys(product).includes(propName)) {
+      propValue.push(product[propName])
     }
-    
+   
 }
-return null;
+  return propValue;
 
   // Change code above this line
 };
-console.log(getProductPrice("Radar"));
-console.log(getProductPrice("Grip"));
-console.log(getProductPrice("Scanner"));
-console.log(getProductPrice("Droid"));
-console.log(getProductPrice("Engine"));
-// ==============================================18/41=========================================
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("category"));
+// ==============================================19/41=========================================
+
