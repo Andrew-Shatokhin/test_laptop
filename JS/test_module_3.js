305117787ddb03d8a7bee52f375088a092f3896e
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-11-28 18:13:05 +0200

    task 22

--- JS/test_module_3.js
+++ JS/test_module_3.js
@@ -428,18 +428,34 @@
 // ==============================================20/41=========================================
 
 // ==============================================21/41=========================================
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const {yesterday, today, tomorrow} = highTemperatures
+
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(meanTemperature);
+// ==============================================21/41=========================================
+
+// ==============================================22/41=========================================
 const highTemperatures = {
   yesterday: 28,
   today: 26,
   tomorrow: 33,
 };
 // Change code below this line
-const {yesterday, today, tomorrow} = highTemperatures
+const { yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+
 
 
 // Change code above this line
 const meanTemperature = (yesterday + today + tomorrow) / 3;
 console.log(meanTemperature);
-// ==============================================21/41=========================================
-
 // ==============================================22/41=========================================
+
+// ==============================================23/41=========================================
