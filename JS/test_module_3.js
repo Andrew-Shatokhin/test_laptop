27ed23d4606229d6661f0b65596fa286e94ad0f9
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-04 17:38:42 +0200

    task 34

--- JS/test_module_3.js
+++ JS/test_module_3.js
@@ -632,20 +632,44 @@
 
 // ==============================================33/41=========================================
 // Change code below this line
-function findMatches(arr, ...args) {
-  const matches = []; // Don't change this line
-  // console.log(args);
-  for (const el of args) {
-    if (arr.includes(el)) {
-      matches.push(el)
-    }
-  }
-  // Change code above this line
-  return matches;
-};
-console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
-console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
-console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41));
-console.log(findMatches([63, 11, 8, 29], 4, 7, 16));
+// function findMatches(arr, ...args) {
+//   const matches = []; // Don't change this line
+//   // console.log(args);
+//   for (const el of args) {
+//     if (arr.includes(el)) {
+//       matches.push(el)
+//     }
+//   }
+//   // Change code above this line
+//   return matches;
+// };
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+// console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+// console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41));
+// console.log(findMatches([63, 11, 8, 29], 4, 7, 16));
 // ==============================================33/41=========================================
 
+// ==============================================34/41=========================================
+
+const bookShelf = {
+  // Change code below this line
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`
+  },
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`
+  }
+  // Change code above this line
+};
+console.log(bookShelf.removeBook("Red sunset"));
+console.log(bookShelf.updateBook("Sands of dune", "Dune"));
+// ==============================================34/41=========================================
+
+// ==============================================35/41=========================================
