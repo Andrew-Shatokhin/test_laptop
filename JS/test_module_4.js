b6e8c150324af5874854b63b8097a12485fc5eb9
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-07 19:11:59 +0200

    m4_task 10

--- JS/test_module_4.js
+++ JS/test_module_4.js
@@ -162,12 +162,31 @@
 
 // ==============================================9/48=========================================
 // Change code below this line
-const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
-// Change code above this line
-console.log(calculateTotalPrice(5, 100));
-console.log(calculateTotalPrice(8, 60));
-console.log(calculateTotalPrice(3, 400));
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// // Change code above this line
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+// ==============================================9/48=========================================
 
+// ==============================================10/48=========================================
+// Change code below this line
+const calculateTotalPrice = (orderedItems) => {
+  let totalPrice = 0;
+
+  orderedItems.forEach(item => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}
+// Change code above this line
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// ==============================================10/48=========================================
+
+// ==============================================11/48=========================================
 
 
 
