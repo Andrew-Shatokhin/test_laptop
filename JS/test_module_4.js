47533eee763b1c293c0850ce873071ad3dca42ff
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-07 16:31:04 +0200

    m4_task 3

--- JS/test_module_4.js
+++ JS/test_module_4.js
@@ -14,21 +14,35 @@
 
 // ==============================================2/48=========================================
 
-function deliverPizza(pizzaName) {
-  return `Delivering ${pizzaName} pizza.`;
-}
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
 
-function makePizza(pizzaName) {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
-}
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
 
-// Chande code below this line
-function makeMessage(pizzaName, callback) {
-  return callback(pizzaName);
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
   
-};
-console.log(makeMessage("Royal Grand", makePizza));
-console.log(makeMessage("Ultracheese", deliverPizza));
+// };
+// console.log(makeMessage("Royal Grand", makePizza));
+// console.log(makeMessage("Ultracheese", deliverPizza));
 // ==============================================2/48=========================================
 
 // ==============================================3/48=========================================
+
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
+
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
+
+makePizza("Ultracheese", function eatPizza(pizzaName) {
+  console.log(`Eating pizza ${pizzaName}`)
+});
