6a63967d1dc56a60d2fcd8e75c580208a939282b
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-07 14:56:47 +0200

    m4_task 2

--- JS/test_module_4.js
+++ JS/test_module_4.js
@@ -1,13 +1,34 @@
-console.log("hello from module 4")
+// console.log("hello from module 4")
 // ==============================================1/48=========================================
-function makePizza() {
-  return "Your pizza is being prepared, please wait.";
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+
+// const result = makePizza();
+// const pointer = makePizza;
+// console.log(result);
+
+// console.log(pointer)
+// ==============================================1/48=========================================
+
+// ==============================================2/48=========================================
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
 }
-// Change code below this line
 
-const result = makePizza();
-const pointer = makePizza;
-console.log(result);
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
 
-console.log(pointer)
-// ==============================================1/48=========================================
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+  
+};
+console.log(makeMessage("Royal Grand", makePizza));
+console.log(makeMessage("Ultracheese", deliverPizza));
+// ==============================================2/48=========================================
+
+// ==============================================3/48=========================================
