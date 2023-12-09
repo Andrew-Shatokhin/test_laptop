86c7982b3e589d843172b2f88e205ccabb29f20d
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-09 11:47:57 +0200

    m4_task 16

--- JS/test_module_4.js
+++ JS/test_module_4.js
@@ -257,27 +257,50 @@
 // ==============================================14/48=========================================
 
 // ==============================================15/48=========================================
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(book => book.title);
+// console.log(titles)
+// ==============================================15/48=========================================
+
+// ==============================================16/48=========================================
 const books = [
   {
     title: "The Last Kingdom",
     author: "Bernard Cornwell",
-    rating: 8.38,
+    genres: ["adventure", "history"],
   },
   {
     title: "Beside Still Waters",
     author: "Robert Sheckley",
-    rating: 8.51,
+    genres: ["fiction"],
   },
   {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism"],
   },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 ];
 // Change code below this line
 
-const titles = books.map(book => book.title);
-console.log(titles)
-// ==============================================15/48=========================================
+const genres = books.flatMap(book => book.genres);
+console.log(genres);
