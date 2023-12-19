e2dcc3787ce3dc9994d8b44c6099942ac4e219b3
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-19 17:56:03 +0200

    m4_task 34

--- JS/test_module_4.js
+++ JS/test_module_4.js
@@ -701,29 +701,36 @@ const users = [
 // ==============================================31/48=========================================
 
 // ==============================================32/48=========================================
-const firstArray = [26, 94, 36, 18];
-const secondArray = [17, 61, 23];
-const thirdArray = [17, 26, 94, 61, 36, 23, 18];
-// Change below this line
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
 
-const anyElementInFirstIsEven = firstArray.some(number=> number %2 === 0);
-const anyElementInFirstIsOdd = firstArray.some(number=> number %2 !== 0);
+// const anyElementInFirstIsEven = firstArray.some(number=> number %2 === 0);
+// const anyElementInFirstIsOdd = firstArray.some(number=> number %2 !== 0);
 
-const anyElementInSecondIsEven = secondArray.some(number=> number %2 === 0);
-const anyElementInSecondIsOdd = secondArray.some(number=> number %2 !== 0);
+// const anyElementInSecondIsEven = secondArray.some(number=> number %2 === 0);
+// const anyElementInSecondIsOdd = secondArray.some(number=> number %2 !== 0);
 
-const anyElementInThirdIsEven = thirdArray.some(number=> number %2 === 0);
-const anyElementInThirdIsOdd = thirdArray.some(number=> number %2 !== 0);
+// const anyElementInThirdIsEven = thirdArray.some(number=> number %2 === 0);
+// const anyElementInThirdIsOdd = thirdArray.some(number=> number %2 !== 0);
 
-console.log(anyElementInFirstIsEven);
-console.log(anyElementInFirstIsOdd);
+// console.log(anyElementInFirstIsEven);
+// console.log(anyElementInFirstIsOdd);
 
-console.log(anyElementInSecondIsEven);
-console.log(anyElementInSecondIsOdd);
+// console.log(anyElementInSecondIsEven);
+// console.log(anyElementInSecondIsOdd);
 
-console.log(anyElementInThirdIsEven);
-console.log(anyElementInThirdIsOdd);
+// console.log(anyElementInThirdIsEven);
+// console.log(anyElementInThirdIsOdd);
 // ==============================================32/48=========================================
 
 // ==============================================33/48=========================================
 
+// Change code below this line
+const isAnyUserActive = users => users.some(user => user.isActive);
+// Change code above this line
+console.log(isAnyUserActive(users));
+// ==============================================33/48=========================================
+
+// ==============================================34/48=========================================
