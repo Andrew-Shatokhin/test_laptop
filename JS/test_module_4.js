d1ade535e8f8c88b54555c8bed4a3ef766e7e2a0
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2023-12-21 21:35:19 +0200

    m4_task 45

--- JS/test_module_4.js
+++ JS/test_module_4.js
@@ -890,9 +890,41 @@ const users = [
 // ==============================================43/48=========================================
 
 // ==============================================44/48=========================================
-const sortByName = users => [...users].sort((a, b)=> a.name.localeCompare(b.name));
-// Change code above this line
-console.log(sortByName(users));
+// const sortByName = users => [...users].sort((a, b)=> a.name.localeCompare(b.name));
+// // Change code above this line
+// console.log(sortByName(users));
 // ==============================================44/48=========================================
 
 // ==============================================45/48=========================================
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+// Change code below this line
+
+// const names = books.filter(rating => rating > MIN_BOOK_RATING).map(book => book.author);
+const names = books.filter(book => book.rating > MIN_BOOK_RATING).map(book => book.author).sort((a, b)=> a.localeCompare(b));
+console.log(names);
+// ==============================================45/48=========================================
+
+// ==============================================46/48=========================================
