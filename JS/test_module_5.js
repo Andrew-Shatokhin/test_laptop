e2494c96489032ffb1971df5abfeb107683086dc
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2024-01-27 10:43:29 +0200

    m5_task 8

--- JS/test_module_5.js
+++ JS/test_module_5.js
@@ -141,21 +141,48 @@
 //  ==============================================6/20=========================================
 
 //  ==============================================7/20=========================================
-class Car {
-  // Change code below this line
-  constructor(brand, model, price) {
-    this.brand = brand,
-    this.model = model,
-    this.price = price
-}
+// class Car {
+//   // Change code below this line
+//   constructor(brand, model, price) {
+//     this.brand = brand,
+//     this.model = model,
+//     this.price = price
+// }
 
 
-  // Change code above this line
-};
+//   // Change code above this line
+// };
 
-console.log(new Car("Audi", "Q3", 36000));
-console.log(new Car("BMW", "X5", 58900));
-console.log(new Car("Nissan","Murano", 31700));
+// console.log(new Car("Audi", "Q3", 36000));
+// console.log(new Car("BMW", "X5", 58900));
+// console.log(new Car("Nissan","Murano", 31700));
 //  ==============================================7/20=========================================
 
 //  ==============================================8/20=========================================
+class Car {
+  // Change code below this line
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code above this line
+}
+const audi = new Car({
+  brand: "Audi",
+  model: "Q3",
+  price: 36000
+});
+const bmw = new Car({
+  brand: "BMW",
+  model: "X5",
+  price: 58900
+});
+const nissan = new Car({
+  brand: "Nissan",
+  model: "Murano",
+  price: 31700
+});
+console.log(audi);
+console.log(bmw);
+console.log(nissan);
