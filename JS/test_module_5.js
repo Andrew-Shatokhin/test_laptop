b38bb34bfd4be15a494ee65c55f455db530129c3
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2024-01-27 10:04:14 +0200

    m5_task 4

--- JS/test_module_5.js
+++ JS/test_module_5.js
@@ -60,36 +60,52 @@
 //  ==============================================2/20=========================================
 
 //  ==============================================3/20=========================================
-const historyService = {
-  orders: [
-    { email: "jacob@hotmail.com", dish: "Burrito" },
-    { email: "solomon@topmail.net", dish: "Burger" },
-    { email: "artemis@coldmail.net", dish: "Pizza" },
-    { email: "solomon@topmail.net", dish: "Apple pie" },
-    { email: "jacob@hotmail.com", dish: "Taco" },
-  ],
-  // Change code below this line
-  getOrdersLog() {
-    return this.orders
-      .map(order => `email: ${order.email} dish: ${order.dish}`)
-      .join(" - ");
-  },
-  getEmails() {
-    const emails = this.orders.map(order => order.email);
-    const uniqueEmails = new Set(emails);
-    return [...uniqueEmails];
-  },
-  getOrdersByEmail(email) {
-    return this.orders.filter(order => order.email === email);
-  },
-  // Change code above this line
-};
-console.log(historyService.getOrdersLog());
-console.log(historyService.getEmails());
-console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return this.orders
+//       .map(order => `email: ${order.email} dish: ${order.dish}`)
+//       .join(" - ");
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
+// console.log(historyService.getOrdersLog());
+// console.log(historyService.getEmails());
+// console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
 //  ==============================================3/20=========================================
 
 //  ==============================================4/20=========================================
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
+};
+// Change code below this line
 
+const child = Object.create(parent);
 
+// Change code above this line
+child.name = "Jason";
+child.age = 27;
 
+console.log('parent:', parent);
+console.log('child:', child);
+//  ==============================================4/20=========================================
+
+//  ==============================================5/20=========================================
