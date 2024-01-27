93f66902d68bcd78232e5ed27cebfb8b84a02b21
Andrew-Shatokhin <shatokhin.a.p@gmail.com>
2024-01-27 10:58:52 +0200

    m5_task 10

--- JS/test_module_5.js
+++ JS/test_module_5.js
@@ -189,23 +189,51 @@
 //  ==============================================8/20=========================================
 
 //  ==============================================9/20=========================================
-class Car {
-  constructor({ brand, model, price }) {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-  }
-  // Change code below this line
-  getPrice() {
-    return this.price;
-  }
-  changePrice(newPrice) {
-    this.price = newPrice
-  }
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice
+//   }
 
 
   // Change code above this line
-}
+// }
 //  ==============================================9/20=========================================
 
 //  ==============================================10/20=========================================
+class Storage{
+  constructor(items) {
+    this.items = items
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem)
+  }
+  removeItem(itemToRemove) {
+    const idx = this.items.indexOf(itemToRemove);
+    // console.log(idx);
+    this.items.splice(idx, 1);
+    // return this.items;
+  }
+}
+
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+//  ==============================================10/20=========================================
+
+//  ==============================================11/20=========================================
